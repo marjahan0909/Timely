c2086c76b3f535ec1d9298a62f016daa8bc6d016
marjahan0909 <154087176+marjahan0909@users.noreply.github.com>
2024-11-09 23:08:55 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TIMELY</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container text-center mt-5">
+        <h2>TIMELY</h2>
+        <div class="login-container">
+            <p>Welcome to Timely, your scheduling solution.</p>
+            <div class="mt-4">
+                <!-- Button for Registration -->
+                <a href="r-select.php" class="btn btn-primary btn-sm me-2">Registration</a>
+                <span class="btn btn-outline-primary btn-sm disabled">or</span>
+                <!-- Button for Login -->
+                <a href="login.php" class="btn btn-primary btn-sm me-2">Log in</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
